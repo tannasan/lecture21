c63fd8435429e72e823709ebf81946f5d0f7e3bd
Gui <brunosilvestrin@brunos-macbook-air.local>
2016-02-01 19:03:21 -0500

    Initial Commit

---
+++ lecture21/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lecture21
+//
+//  Created by Bruno Silvestrin on 2016-02-01.
+//  Copyright © 2016 Bruno Silvestrin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
